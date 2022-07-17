9bae754ff788f99450eb3cc5f9a46a24e180fe39
elayne <elayne.natalia@outlook.com>
2022-07-17 16:23:14 -0300

    entidades e conexão com banco

---
+++ GeekShopping.ProductAPI/Startup.cs
@@ -0,0 +1,65 @@
+﻿using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI
+{
+    public class Startup
+    {
+            public IConfiguration Configuration { get; }
+            public IWebHostEnvironment Environment { get; }
+
+            public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+            {
+                Configuration = configuration;
+                Environment = environment;
+            }
+
+            public void ConfigureServices(IServiceCollection services)
+            {
+                services.AddCors();
+                services.AddControllers();
+
+                string stringConnection = Configuration["MySQLConnection:MySQLConnection"];
+
+                services.AddDbContext<MySqlContext>(options =>
+                            options.UseMySql(stringConnection, 
+                                            new MySqlServerVersion(
+                                                new Version(8,0,5))));
+            }
+
+            public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+            {
+                if (env.IsDevelopment())
+                {
+                    app.UseDeveloperExceptionPage();
+                }
+
+                app.UseHttpsRedirection();
+                app.UseRouting();
+
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "API REST with ASP NET CORE 3.1");
+                    c.RoutePrefix = "swagger";
+                });
+
+                app.UseSwagger(c =>
+                {
+                    c.SerializeAsV2 = true;
+                });
+
+                app.UseCors(x => x
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+
+                app.UseAuthentication();
+                app.UseAuthorization();
+
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                });
+            }
+        }
+}
